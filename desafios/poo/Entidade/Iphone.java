3c2037549abb5a9e77abd2c00a88ca7231cf68f9
Aluanna <aluanna250@gmail.com>
2023-10-22 10:21:29 -0300

    1

---
+++ desafios/poo/Entidade/Iphone.java
@@ -0,0 +1,32 @@
+package Entidade;
+
+public class Iphone {
+
+    public void ligar(String string) {
+    }
+
+    public void atender() {
+    }
+
+    public void iniciarCorreioVoz() {
+    }
+
+    public void tocar(String string) {
+    }
+
+    public void pausar() {
+    }
+
+    public void selecionarMusica() {
+    }
+
+    public void exibirPagina() {
+    }
+
+    public void adicionarNovaAba() {
+    }
+
+    public void atualizarPagina() {
+    }
+    
+}
